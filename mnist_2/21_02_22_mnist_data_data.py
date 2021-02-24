f0cedf9b095e2c762397ebd42485a3f003cc98b5
skanwngud <skanwngud@gmail.com>
2021-02-24 17:34:52 +0900

    commit

--- mnist_2/21_02_22_mnist_data_data.py
+++ mnist_2/21_02_22_mnist_data_data.py
@@ -41,6 +41,8 @@ answer_csv=pd.read_csv('c:/data/dacon/data2/dirty_mnist_answer.csv', index_col=0
 df_train=pd.read_csv('c:/data/dacon/data2/mnist_data/train.csv', index_col=0, header=0)
 dftrain=df_train.iloc[:, 2:].values # mnist1 x
 dfanswer=df_train.iloc[:, 1].values # mnist1 y
+df_test=pd.read_csv('c:/data/dacon/data2/mnist_data/test.csv', index_col=0, header=0)
+dftest=df_test.iloc[:, 1:].values
 
 # digit OneHotEncoding
 label=list()
@@ -128,7 +130,8 @@ for i in range(len(dfanswer)):
 
 label=to_categorical(label)
 
-print(label)
+print(dftrain[:5])
+print(label[:5])
 
 # print(len(train_list)) # 50000
 # print(len(test_list)) # 5000
@@ -143,230 +146,286 @@ x=dftrain
 y=label
 
 x=x.reshape(-1, 28, 28, 1)/255.
+dftest=dftest.reshape(-1, 28, 28, 1)/255.
 
-print(y.shape)
-
-x_train, x_val, y_train, y_val=train_test_split(
-    x, y,
-    train_size=0.8,
-    random_state=23
-)
-
-x_train, x_test, y_train, y_test=train_test_split(
-    x_train, y_train,
-    train_size=0.9,
-    random_state=23
-)
-
-print(x_test)
-print(y_test)
-
-# print(x_train.shape) # (1474, 28, 28, 1)
-# print(x_val.shape) # (410, 28, 28, 1)
-# print(x_test.shape) # (164, 28, 28, 1)
-# print(y_train.shape) # (1474, )
-
-model=Sequential()
-model.add(Conv2D(128, 2, padding='same', input_shape=(28, 28, 1)))
-model.add(BatchNormalization())
-model.add(Activation('relu'))
-model.add(Conv2D(128, 2, padding='same'))
-model.add(BatchNormalization())
-model.add(Activation('relu'))
-model.add(Conv2D(128, 2, padding='same'))
-model.add(BatchNormalization())
-model.add(Activation('relu'))
-model.add(MaxPooling2D())
-model.add(Dropout(0.2))
-model.add(Conv2D(64, 2, padding='same'))
-model.add(BatchNormalization())
-model.add(Activation('relu'))
-model.add(Conv2D(64, 2, padding='same'))
-model.add(BatchNormalization())
-model.add(Activation('relu'))
-model.add(MaxPooling2D())
-model.add(Dropout(0.2))
-model.add(Flatten())
-model.add(Dense(512))
-model.add(BatchNormalization())
-model.add(Activation('relu'))
-model.add(Dense(256))
-model.add(Dense(128))
-model.add(Dense(26, activation='softmax'))
-
-es=EarlyStopping(
-    patience=100,
-    verbose=1
-)
-
-mc=ModelCheckpoint(
-    'c:/data/modelcheckpoint/dacon2.hdf5',
-    verbose=1,
-    save_best_only=True
-)
-
-rl=ReduceLROnPlateau(
-    verbose=1,
-    factor=0.5,
-    patience=50
-)
-
-model.compile(
-    optimizer=Adam(
-        learning_rate=0.1),
-    loss='categorical_crossentropy',
-    metrics='acc'
-)
-
-model.fit(
-    x_train, y_train,
-    validation_data=(x_val, y_val),
-    batch_size=32,
-    epochs=1000,
-    callbacks=[es, mc, rl]
-)
-
-loss=model.evaluate(
-    x_test, y_test
-)
-
-pred=model.predict(
-    x_test
-)
-
-pred=np.argmax(pred)
-
-print(y_test.shape)
-print(x_test.shape)
-
-print(loss)
-print(pred)
-print(pred[0])
-# print(y_test[:5])
-
-'''
-train_numpy=list()
-test_numpy=list()
-
-for i in train_list:
-    img=cv2.imread(i, cv2.IMREAD_GRAYSCALE)
-    img=cv2.resize(img, (128, 128))
-    img=np.array(img)/255.
-    train_numpy.append(img)
-
-
-for i in test_list:
-    img=cv2.imread(i, cv2.IMREAD_GRAYSCALE)
-    img=cv2.resize(img, (128, 128))
-    img=np.array(img)/255.
-    test_numpy.append(img)
-
-print(len(train_numpy))
-print(train_numpy[0])
-print(train_numpy[0].shape)
-
-train_list=np.array(train_numpy)
-test_list=np.array(test_numpy)
-answer_list=answer_csv.to_numpy()
-
-plt.imshow(train_numpy[0])
-plt.show()
-
-np.save('c:/data/dacon/data2/train.npy', arr=train_numpy)
-np.save('c:/data/dacon/data2/test.npy', arr=test_numpy)
-np.save('c:/data/dacon/data2/answer.npy', arr=answer_list)
-
-
-# npy 로드
-train=np.load('c:/data/dacon/data2/train.npy') # x
-answer=np.load('c:/data/dacon/data2/answer.npy') # y
-test=np.load('c:/data/dacon/data2/test.npy')
-
-
-
-# print(train.shape) # (50000, 128, 128)
-# print(answer.shape) # (50000, 26)
-# print(test.shape) # (5000, 128, 128)
+# print(y.shape)
 
 # x_train, x_val, y_train, y_val=train_test_split(
-#     train, answer,
+#     x, y,
 #     train_size=0.8,
 #     random_state=23
 # )
 
 # x_train, x_test, y_train, y_test=train_test_split(
 #     x_train, y_train,
-#     train_size=0.8,
+#     train_size=0.9,
 #     random_state=23
 # )
 
-# print(x_train.shape) # (40000, 128, 128)
-# print(y_train.shape) # (10000, 128, 128)
-# print(x_val.shape)
-# print(x_test.shape)
+datagen=ImageDataGenerator(
+    width_shift_range=0.1,
+    height_shift_range=0.1,
+    vertical_flip=True,
+    horizontal_flip=True,
+    rotation_range=0.1
+)
 
-# # 데이터 분리
+batch=16
 
-for train_index, val_index in kf.split(train, answer):
-    x_train=train[train_index]
-    y_train=answer[train_index]
-    x_val=train[val_index]
-    y_val=answer[val_index]
+datagen2=ImageDataGenerator()
 
-    x_train, x_test, y_train, y_test=train_test_split(
+# train_set=datagen.flow(
+#     x_train, y_train,
+#     batch_size=batch,
+#     seed=23
+# )
+
+# val_set=datagen2.flow(
+#     x_val, y_val,
+#     seed=23
+# )
+
+# test_set=datagen2.flow(
+#     x_test, y_test,
+#     seed=23
+# )
+
+# print(x_test[:5])
+# print(y_test)
+
+# print(x_train.shape) # (1474, 28, 28, 1)
+# print(x_val.shape) # (410, 28, 28, 1)
+# print(x_test.shape) # (164, 28, 28, 1)
+# print(y_train.shape) # (1474, )
+pred=list()
+pred=0
+kf=KFold(
+    n_splits=5,
+    shuffle=True,
+    random_state=23
+)
+
+i=1
+
+for train_index, val_index in kf.split(x, y):
+    x_train=x[train_index]
+    y_train=y[train_index]
+    x_val=x[val_index]
+    y_val=y[val_index]
+
+    train_set=datagen.flow(
         x_train, y_train,
-        train_size=0.9,
-        random_state=23
+        seed=23,
+        batch_size=batch
     )
 
-    x_train=x_train.reshape(-1, 128, 128, 1)
-    x_val=x_val.reshape(-1, 128, 128, 1)
-    x_test=x_test.reshape(-1, 128, 128, 1)
+    val_set=datagen2.flow(
+        x_val, y_val,
+        seed=23
+    )
+
+    test_set=datagen2.flow(
+        dftest,
+        seed=23
+    )
 
     model=Sequential()
-    model.add(Conv2D(32, 2, padding='same', input_shape=(128, 128, 1)))
+    model.add(Conv2D(128, 2, padding='same', input_shape=(28, 28, 1)))
     model.add(BatchNormalization())
     model.add(Activation('relu'))
-    model.add(Conv2D(32, 2, padding='same'))
+    model.add(Conv2D(128, 2, padding='same'))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(Conv2D(128, 2, padding='same'))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D())
+    model.add(Dropout(0.2))
+    model.add(Conv2D(64, 2, padding='same'))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(Conv2D(64, 2, padding='same'))
     model.add(BatchNormalization())
     model.add(Activation('relu'))
     model.add(Conv2D(64, 2, padding='same'))
     model.add(BatchNormalization())
     model.add(Activation('relu'))
     model.add(MaxPooling2D())
-    model.add(Dropout(0.4))
+    model.add(Dropout(0.2))
+    model.add(Conv2D(32, 2, padding='same'))
+    model.add(Conv2D(32, 2, padding='same'))
     model.add(Flatten())
-    model.add(Dense(1024))
-    model.add(Dense(52))
-    model.add(Reshape((26, 2)))
-    model.add(Dense(2, activation='softmax'))
+    model.add(Dense(512))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(Dense(256))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(Dense(128))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(Dense(26, activation='softmax'))
+
+    es=EarlyStopping(
+        patience=200,
+        verbose=1
+    )
+
+    mc=ModelCheckpoint(
+        'c:/data/modelcheckpoint/dacon2.hdf5',
+        verbose=1,
+        save_best_only=True
+    )
+
+    rl=ReduceLROnPlateau(
+        verbose=1,
+        factor=0.5,
+        patience=50
+    )
 
     model.compile(
-        optimizer='adam',
-        loss='sparse_categorical_crossentropy',
+        optimizer=Adam(
+            learning_rate=0.01),
+        loss='categorical_crossentropy',
         metrics='acc'
     )
 
+    # model.fit(
+    #     x_train, y_train,
+    #     validation_data=(x_val, y_val),
+    #     batch_size=16,
+    #     epochs=5000,
+    #     callbacks=[es, mc, rl]
+    # )
+
     model.fit(
-        x_train, y_train,
-        validation_data=(x_val, y_val),
-        batch_size=6,
-        epochs=1
+        train_set,
+        validation_data=val_set,
+        steps_per_epoch=len(x_train)//batch,
+        epochs=1000,
+        callbacks=[es, mc, rl]
     )
 
-    loss=model.evaluate(
-        x_test, y_test
-    )
+    # loss=model.evaluate(
+    #     test_set
+    # )
+
+    model.load_weights('c:/data/modelcheckpoint/dacon2.hdf5')
 
     pred=model.predict(
-        x_test
+        test_set
     )
-    print(np.argmax(pred[:5], axis=-1))
 
-    print(loss)
-# print(y_test[:5])
+    pred=np.argmax(pred, axis=-1)
+
+
+    results=list()
+
+    for i in range(len(pred)):
+        if pred[i]==0:
+            img='A'
+            results.append(img)
+        elif pred[i]==1:
+            img='B'
+            results.append(img)
+        elif pred[i]==2:
+            img='C'
+            results.append(img)
+        elif pred[i]==3:
+            img='D'
+            results.append(img)
+        elif pred[i]==4:
+            img='E'
+            results.append(img)
+        elif pred[i]==5:
+            img='F'
+            results.append(img)
+        elif pred[i]==6:
+            img='G'
+            results.append(img)
+        elif pred[i]==7:
+            img='H'
+            results.append(img)
+        elif pred[i]==8:
+            img='I'
+            results.append(img)
+        elif pred[i]==9:
+            img='J'
+            results.append(img)
+        elif pred[i]==10:
+            img='K'
+            results.append(img)
+        elif pred[i]==11:
+            img='L'
+            results.append(img)
+        elif pred[i]==12:
+            img='M'
+            results.append(img)
+        elif pred[i]==13:
+            img='N'
+            results.append(img)
+        elif pred[i]==14:
+            img='O'
+            results.append(img)
+        elif pred[i]==15:
+            img='P'
+            results.append(img)
+        elif pred[i]==16:
+            img='Q'
+            results.append(img)
+        elif pred[i]==17:
+            img='R'
+            results.append(img)
+        elif pred[i]==18:
+            img='S'
+            results.append(img)
+        elif pred[i]==19:
+            img='T'
+            results.append(img)
+        elif pred[i]==20:
+            img='U'
+            results.append(img)
+        elif pred[i]==21:
+            img='V'
+            results.append(img)
+        elif pred[i]==22:
+            img='W'
+            results.append(img)
+        elif pred[i]==23:
+            img='X'
+            results.append(img)
+        elif pred[i]==24:
+            img='Y'
+            results.append(img)
+        elif pred[i]==25:
+            img='Z'
+            results.append(img)
+
+    print('results : ', results[:5])
+
+plt.imshow(results[0])
+plt.show()
+
+# print('loss, acc : ', loss)
 
 # results
-# try : [0.6907089948654175, 0.5378028750419617]
-# try 2: [0.6905454993247986, 0.5378028750419617]
-'''
+# loss, acc :  [0.8030679225921631, 0.7439024448394775]
+# reults :  ['S', 'W', 'D', 'S', 'T']
+
+# loss, acc :  [0.6848068237304688, 0.7439024448394775]
+# reults :  ['S', 'W', 'H', 'Q', 'H']
+
+# loss, acc :  [0.7705674767494202, 0.792682945728302]
+# reults :  ['S', 'W', 'D', 'Q', 'H']
+
+# reults :  ['S', 'E', 'A', 'B', 'T']
+# loss, acc :  [0.0, 0.0]
+
+# reults :  ['P', 'M', 'Q', 'H', 'L']
+# reults :  ['V', 'M', 'Q', 'Y', 'L']
+# reults :  ['V', 'M', 'Y', 'Y', 'L']
+# reults :  ['V', 'W', 'Q', 'Y', 'L']
+# reults :  ['V', 'M', 'R', 'Y', 'L']
+
+# results :  ['V', 'W', 'Y', 'Y', 'L']
+# results :  ['V', 'M', 'Z', 'Y', 'L']

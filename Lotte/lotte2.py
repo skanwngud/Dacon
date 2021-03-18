35c0ebd1461e811a0caa8529eb423508110625bf
skanwngud <skanwngud@gmail.com>
2021-03-18 21:48:56 +0900

    commit

--- Lotte/lotte2.py
+++ Lotte/lotte2.py
@@ -7,15 +7,11 @@ from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential
 from keras.layers import Dense, Conv2D, MaxPooling2D, Flatten,\
     BatchNormalization, Activation, Dropout
-from tensorflow.keras.applications import MobileNet
+from tensorflow.keras.applications import MobileNet, EfficientNetB4
 from keras.callbacks import EarlyStopping, ReduceLROnPlateau, ModelCheckpoint
 from keras.optimizers import Adam
 
 from sklearn.model_selection import train_test_split, KFold
-
-# test = np.load(
-#     'c:/LPD_competition/test.npy'
-# )
 
 test = list()
 for i in range(72000):
@@ -23,124 +19,43 @@ for i in range(72000):
         'c:/LPD_competition/test/%s.jpg'%i
     )
     temp = cv2.resize(temp, (128, 128))
-    temp = np.array(temp)/255.
+    temp = np.array(temp)
     test.append(temp)
 
 submission = pd.read_csv(
     'c:/LPD_competition/sample.csv'
 )
 
-batch_size = 16
+train = list()
+label = list()
+for i in range(1000):
+    for j in range(48):
+        temp = cv2.imread(
+            'c:/LPD_competition/train/' + str(i) +'/'+ str(j) + '.jpg'
+        )
+        temp = cv2.resize(temp, (128, 128))
+        temp = np.array(temp)
+        train.append(temp)
+        label.append(i)
 
-es = EarlyStopping(
-    patience=100,
-    verbose=1
+test = np.array(test)/255.
+train = np.array(train)/255.
+label = np.array(label)
+
+print(test.shape)
+print(train.shape)
+print(label.shape)
+
+print(label[:50])
+
+np.save(
+    'c:/data/npy/lotte_x.npy', arr = train
 )
 
-rl = ReduceLROnPlateau(
-    patience=10,
-    verbose=1,
-    factor=0.5
+np.save(
+    'c:/data/npy/lotte_y.npy', arr = label
 )
 
-mc = ModelCheckpoint(
-    'c:/data/modelcheckpoint/lotte.hdf5',
-    verbose=1,
-    save_best_only=True
-)
-
-datagen = ImageDataGenerator(
-    vertical_flip=True,
-    horizontal_flip=True,
-    width_shift_range=(-1, 1),
-    height_shift_range=(-1,1),
-    validation_split=0.2
-)
-
-datagen2 = ImageDataGenerator()
-
-train_set = datagen.flow_from_directory(
-    'c:/LPD_competition/train/',
-    target_size=(128, 128),
-    subset='training',
-    batch_size=batch_size
-)
-
-val_set = datagen.flow_from_directory(
-    'c:/LPD_competition/train/',
-    target_size=(128, 128),
-    subset='validation',
-    batch_size=batch_size
-)
-
-test_set = datagen2.flow(
-    test,
-    batch_size=batch_size,
-    shuffle=False
-)
-
-x_train = train_set[0][0]
-y_train = train_set[0][1]
-x_val = val_set[0][0]
-y_val = val_set[0][1]
-
-print(x_train.shape)
-print(y_train.shape)
-print(x_val.shape)
-print(y_val.shape)
-
-epochs = len(train_set)//batch_size
-
-eff = MobileNet(
-    include_top=False,
-    input_shape=(128, 128, 3)
-)
-eff.trainable=True
-# modeling
-model = Sequential()
-model.add(eff)
-model.add(Conv2D(128, 3, padding='same'))
-model.add(BatchNormalization())
-model.add(Activation('relu'))
-model.add(MaxPooling2D(3, padding='same'))
-model.add(Conv2D(256, 3, padding='same'))
-model.add(BatchNormalization())
-model.add(Activation('relu'))
-model.add(MaxPooling2D(3, padding='same'))
-model.add(Flatten())
-model.add(Dense(512, activation='relu'))
-model.add(Dense(1024, activation='relu'))
-model.add(Dense(1000, activation='softmax'))
-
-# compile, fitting
-model.compile(
-    optimizer=Adam(
-        learning_rate=0.001
-    ),
-    loss='categorical_crossentropy',
-    metrics='acc'
-)
-
-model.fit(
-    x_train, y_train,
-    validation_data=(x_val, y_val),
-    epochs = 1000,
-    steps_per_epoch=epochs,
-    callbacks=[es, rl]
-)
-
-# predict
-# model.load_weights(
-#     'c:/data/modelcheckpoint/lotte.hdf5'
-# )
-
-pred = model.predict(
-    test_set
-)
-
-submission['prediction']=np.argmax(pred, axis=-1)
-
-submission.to_csv(
-    'c:/LPD_competition/submission.csv',
-    index=False
+np.save(
+    'c:/data/npy/lotte_test.npy', arr = test
 )
---
+++ Lotte/lotte3.py
@@ -0,0 +1,108 @@
+import numpy as np
+import pandas as pd
+import warnings
+warnings.filterwarnings('ignore')
+
+from sklearn.model_selection import KFold, train_test_split
+
+from tensorflow.keras.applications import MobileNet, EfficientNetB4
+from tensorflow.keras.models import Model, Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten,\
+    BatchNormalization, Activation, Dense, Dropout, Input
+from tensorflow.keras.callbacks import EarlyStopping, ReduceLROnPlateau, ModelCheckpoint
+
+es = EarlyStopping(
+    patience=100,
+    verbose=1
+)
+
+rl = ReduceLROnPlateau(
+    patience=30,
+    verbose=1
+)
+
+mc = ModelCheckpoint(
+    'c:/data/modelcheckpoint/lotte.hdf5',
+    save_best_only=True,
+    verbose=1
+)
+
+x = np.load(
+    'c:/data/npy/lotte_x.npy'
+)
+
+y = np.load(
+    'c:/data/npy/lotte_y.npy'
+)
+
+test = np.load(
+    'c:/data/npy/lotte_test.npy'
+)
+
+submission = pd.read_csv(
+    'c:/LPD_competition/sample.csv'
+)
+
+mob = MobileNet(
+    include_top=False,
+    input_shape=(128, 128, 3)
+)
+
+eff = EfficientNetB4(
+    include_top=False,
+    input_shape=(128, 128, 3)
+)
+
+mob.trainable = True
+eff.trainable = True
+
+x_train, x_val, y_train, y_val = train_test_split(
+    x, y, train_size=0.8, random_state=23
+)
+
+model = Sequential()
+model.add(mob)
+model.add(Conv2D(128, 3, padding='same'))
+model.add(BatchNormalization())
+model.add(Activation('relu'))
+model.add(MaxPooling2D(3, padding='same'))
+model.add(Conv2D(128, 3, padding='same'))
+model.add(BatchNormalization())
+model.add(Activation('relu'))
+model.add(MaxPooling2D(3, padding='same'))
+model.add(Dense(128, activation='relu'))
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dense(256, activation='relu'))
+model.add(Dense(512, activation='relu'))
+model.add(Dense(1000, activation='softmax'))
+
+model.compile(
+    optimizer='adam',
+    loss = 'sparse_categorical_crossentropy',
+    metrics='acc'
+)
+
+model.fit(
+    x_train, y_train,
+    validation_data=(x_val, y_val),
+    epochs=1000,
+    callbacks=[es, rl, mc]
+)
+
+model.load_weights(
+    'c:/data/modelcheckpoint/lotte.hdf5'
+)
+
+pred = model.predict(
+    test
+)
+
+submission['prediction'] = np.argmax(pred, axis = -1)
+submission.to_csv(
+    'c:/data/csv/lotte.csv',
+    index = False
+)
+
+print(test[:5])
+print(pred[:5])

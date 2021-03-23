1d9b6c0d897e8724363aafc19a23d9204c6d7e4f
skanwngud <skanwngud@gmail.com>
2021-03-23 10:24:11 +0900

    commit

--- Lotte/lotte3.py
+++ Lotte/lotte3.py
@@ -4,26 +4,33 @@ import warnings
 warnings.filterwarnings('ignore')
 import cv2
 import matplotlib.pyplot as plt
+import datetime
 
 from sklearn.model_selection import KFold, train_test_split
 
-from tensorflow.keras.applications import MobileNet, EfficientNetB7
+from tensorflow.keras.applications import MobileNet, EfficientNetB4
 from tensorflow.keras.models import Model, Sequential
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten,\
-    BatchNormalization, Activation, Dense, Dropout, Input, Concatenate, GlobalAveragePooling2D
+    BatchNormalization, Activation, Dense, Dropout, Input, Concatenate, \
+        GlobalAveragePooling2D, Dropout
 from tensorflow.keras.callbacks import EarlyStopping, ReduceLROnPlateau, ModelCheckpoint
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow import train
 
-# datagen = ImageDataGenerator(
-#     vertical_flip=True,
-#     horizontal_flip=True,
-#     rotation_range=0.1,
-#     width_shift_range=(-1, 1),
-#     height_shift_range=(-1, 1),
-#     validation_split=0.8
-# )
+str_time = datetime.datetime.now()
 
-# datagen2 = ImageDataGenerator()
+datagen = ImageDataGenerator(
+    vertical_flip=True,
+    horizontal_flip=True,
+    rotation_range=40,
+    width_shift_range=(-1, 1),
+    height_shift_range=(-1, 1),
+    rescale = 1./255
+)
+
+datagen2 = ImageDataGenerator(
+    rescale=1./255
+)
 
 es = EarlyStopping(
     patience=50,
@@ -42,15 +49,15 @@ mc = ModelCheckpoint(
 )
 
 x = np.load(
-    'c:/data/npy/lotte_x.npy'
+    'c:/data/npy/lotte_x_2.npy'
 )
 
 y = np.load(
-    'c:/data/npy/lotte_y.npy'
+    'c:/data/npy/lotte_y_2.npy'
 )
 
 test = np.load(
-    'c:/data/npy/lotte_test.npy'
+    'c:/data/npy/lotte_test_2.npy'
 )
 
 submission = pd.read_csv(
@@ -62,52 +69,42 @@ submission = pd.read_csv(
 #     input_shape=(128, 128, 3)
 # )
 
-eff = EfficientNetB7(
+eff = EfficientNetB4(
     include_top=False,
-    input_shape=(128, 128, 3)
+    input_shape=(200, 200, 3)
 )
 
 # mob.trainable = True
 eff.trainable = True
 
-# x = x.reshape(-1, 128, 128, 1)
-# test = test.reshape(-1, 128, 128, 1)
-
-batch_size = 32
-epochs = len(x)//batch_size
-
-# x_train = datagen.flow(
-#     x,
-#     batch_size = batch_size,
-#     subset = 'training'
-# )
-
-# x_val = datagen.flow(
-#     x,
-#     batch_size = batch_size,
-#     subset = 'validation'
-# )
-
-# test_set = datagen2.flow(
-#     test,
-#     batch_size=batch_size,
-#     shuffle=False
-# )
-
-# x_train = np.array(x_train)/255.
-# x_val = np.array(x_val)/255.
+batch_size = 16
 
 x_train, x_val, y_train, y_val = train_test_split(
     x, y, train_size=0.8, random_state=23
 )
 
+train_set = datagen.flow(
+    x_train, y_train,
+    seed = 23,
+    batch_size = batch_size
+)
+
+val_set = datagen2.flow(
+    x_val, y_val,
+    seed = 23,
+    batch_size = batch_size
+)
+
+epochs = len(x_train)//batch_size
+
 # 학원에서는 x,test 에 /255. 하고 집에서는 /255. 하지 말 것
 
 model = Sequential()
 model.add(eff)
-model.add(Conv2D(1024, kernel_size=3, padding='same', activation = 'swish'))
 model.add(GlobalAveragePooling2D())
-model.add(Dense(1000, activation='softmax'))
+model.add(Dense(4048, activation = 'swish'))
+model.add(Dropout(0.2))
+model.add(Dense(1000, activation = 'softmax'))
 
 model.compile(
     optimizer='adam',
@@ -116,10 +113,10 @@ model.compile(
 )
 
 hist = model.fit(
-    x_train, y_train,
-    validation_data=(x_val, y_val),
-    epochs=1,
-    # steps_per_epoch=epochs,
+    train_set,
+    validation_data=val_set,
+    epochs=1000,
+    steps_per_epoch=epochs,
     callbacks=[es, rl, mc],
     batch_size = batch_size
 )
@@ -138,4 +135,5 @@ submission.to_csv(
     index = False
 )
 
+print('time : ', datetime.datetime.now() - str_time)
 print('done')

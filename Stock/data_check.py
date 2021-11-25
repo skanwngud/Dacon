0cf76197e3dea729fcac06193b6bdab940e7bc72
skanwngud <skanwngud@gmail.com>
2021-11-25 19:38:19 +0900

    2021-11-25

--- Stock/data_check.py
+++ Stock/data_check.py
@@ -8,22 +8,24 @@ list_name = "stock_list.csv"
 
 stock_list = pd.read_csv(os.path.join(path, list_name))
 
-stock_name = np.array(stock_list['종목명'])
-stock_code = np.array(stock_list['종목코드'])
+stock_name = stock_list['종목명']
+stock_code = stock_list['종목코드'].apply(lambda x:str(x).zfill(6))
 
 stock_dict = {name : value for name, value in zip(stock_name, stock_code)}
 
 print(stock_dict)
 print(list(stock_dict.values()))
+
 str_date = '20200101'
-end_date = '20201231'
+end_date = '20211101'
 
 # for code in list(stock_dict.values()):
     # stock = fdr.DataReader(str(code), str_date, end_date)
 for code in stock_code:
     stock = fdr.DataReader(str(code), str_date, end_date)
+    print(stock)
     print(stock.shape)
     print('next')
 
 
-print(fdr.DataReader(str(list(stock_dict.values()))), str_date, end_date)
+# print(fdr.DataReader('005930', str_date, end_date))
